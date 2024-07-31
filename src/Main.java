d8bbbc8a90cca16b16a660dcc1ba91de4153d571
Black77 <islepuchin@yandex.ru>
2024-07-31 13:06:11 +0300

    init project

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Part 1
+        System.out.println("Задание 1");
+
+        System.out.println();
+
+        // Part 2
+        System.out.println("Задание 2");
+
+        System.out.println();
+
+        // Part 3
+        System.out.println("Задание 3");
+
+        System.out.println();
+
+        // Part 4
+        System.out.println("Задание 4");
+
+        System.out.println();
+
+    }
+}
